64f8bddac9ebd65c58dae5cfb56f93a36bf60595
bautykamijo <bkamijo@udesa.edu.ar>
2023-11-06 17:24:39 -0300

    l

--- proyecto-reactnative-prog3/App.js
+++ proyecto-reactnative-prog3/App.js
@@ -5,6 +5,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import Home from './src/screens/Home';
 import Register from './src/screens/Register';
+import Login from './src/screens/Login';
 
 const Stack = createNativeStackNavigator();
 
