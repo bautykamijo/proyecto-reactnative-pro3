7ffac44d706c611d667b7e9117cc606e67417717
gonzalo-lemoine <glemoine@udesa.edu.ar>
2023-11-08 15:54:41 -0300

    login

--- proyecto-reactnative-prog3/src/screens/Login.js
+++ proyecto-reactnative-prog3/src/screens/Login.js
@@ -3,10 +3,99 @@ import { Text, View, TouchableOpacity, TextInput, StyleSheet } from "react-nativ
 import { auth } from '../firebase/config';
 
 class Login extends Component {
-    
+    constructor (){
+        super();
+        this.state = {
+            email: "",
+            password: "",
+        };
+    }
+
+    componentDidMount() {
+        auth.onAuthStateChanged((user) => {
+            if (user) {
+                this.props.navigation.navigate("Home");
+            }
+        });
+    }
+
+    login (email, pass) {
+        auth.signInWithEmailAndPassword(email, pass)
+        .then ((respnse) => {
+            console.log ("Login si", response);
+            this.props.navigation.navigate("Home");
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+    }
+
+    render() {
+        return (
+            <View style = {StyleSheet.formContainer}>
+                <Text>Login</Text>
+                <TextInput
+                    style={styles.input}
+                    onChangeText={(text) => this.setState({ email: text })}
+                    placeholder="email"
+                    keyboardType="email-address"
+                    value={this.state.email}
+                />
+                <TextInput
+                    style={styles.input}
+                    onChangeText={(text) => this.setState({ password: text })}
+                    placeholder="password"
+                    keyboardType="default"
+                    secureTextEntry={true}
+                    value={this.state.password}
+                />
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => this.login(this.state.email, this.state.password)}
+                    >
+                <Text style={styles.textButton}>Ingresar</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    onPress={() => this.props.navigation.navigate("Register")}
+                    >
+                <Text>No tengo cuenta. Registrarme.</Text>
+                </TouchableOpacity>
+            </View>
+        )
+    }
 }
+const styles = StyleSheet.create({
+    formContainer: {
+      paddingHorizontal: 10,
+      marginTop: 20,
+    },
+    input: {
+      height: 20,
+      paddingVertical: 15,
+      paddingHorizontal: 10,
+      borderWidth: 1,
+      borderColor: "#ccc",
+      borderStyle: "solid",
+      borderRadius: 6,
+      marginVertical: 10,
+    },
+    button: {
+      backgroundColor: "blue",
+      paddingHorizontal: 10,
+      paddingVertical: 6,
+      textAlign: "center",
+      borderRadius: 4,
+      borderWidth: 1,
+      borderStyle: "solid",
+      borderColor: "#28a745",
+    },
+    textButton: {
+      color: "#fff",
+    },
+  });
+  
+  export default Login;
+  
 
 
 
-
-export default Login;
