ed9bb949cbee9c9dd2b54c374805053f34cd2558
bautykamijo <bkamijo@udesa.edu.ar>
2023-11-08 15:54:46 -0300

    confir

--- proyecto-reactnative-prog3/src/screens/Register.js
+++ proyecto-reactnative-prog3/src/screens/Register.js
@@ -17,6 +17,14 @@ class Register extends Component {
         }
     }
 
+    componentDidMount() {
+        auth.onAuthStateChanged((user) => {
+          if (user) {
+            this.props.navigation.navigate("Login");
+          }
+        });
+      }
+
     register(mail, pass){
         auth.createUserWithEmailAndPassword(mail, pass)
          .then( response => {
