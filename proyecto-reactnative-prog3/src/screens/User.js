a57964cf4b9b2b6354d04caa2367b7960cad53e2
gonzalo-lemoine <glemoine@udesa.edu.ar>
2023-11-13 16:01:35 -0300

    user

--- proyecto-reactnative-prog3/src/screens/User.js
+++ proyecto-reactnative-prog3/src/screens/User.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import { Text, View, TouchableOpacity } from "react-native";
+import { auth, db } from "../firebase/config";
 
 class User extends Component {
 
     constructor(props){
         super(props);
-        this.state = {
-            valor : ''
-        }
+        this.state = {}
+    }
+
+    logout() {
+        auth.signOut();
+        this.props.navigation.navigate ("Login")
     }
 
    
@@ -15,7 +19,14 @@ class User extends Component {
     render(){
     return(
         <View>
-            <Text>Hola mundo!</Text>
+            <Text>Nombre de usuario: </Text>
+            <Text>Email: {auth.currentUser.email}</Text>
+            <Text>Mini bio: </Text>
+            <Text>Foto de perfil: </Text>
+           <TouchableOpacity onPress ={() => this.logout()}>
+        
+                <Text>Logout</Text>
+           </TouchableOpacity>
         </View>
     )}
 };
